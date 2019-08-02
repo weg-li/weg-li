1f53f8913112e0818ba13d21ec59eb30c38bd9f0
Eckhard Rotte <erotte@gmail.com>
2019-08-02 16:58:19 +0200

    Liste der Park- und Verkehrsvergehen erweitert und angepasst

--- app/lib/vehicle.rb
+++ app/lib/vehicle.rb
@@ -60,20 +60,28 @@ class Vehicle
 
   def self.charges
     @charges = [
+      'Parken auf Radweg unbeschildert',
+      'Parken auf Radweg beschildert',
+      'Parken auf Fahrradschutzstreifen',
       'Parken im Kreuzungs-/Einmündungsbereich sowie 5 Meter davor und dahinter',
+      'Parken auf dem Fußgängerüberweg sowie 5 Meter davor und dahinter',
+      'Parken im Halteverbot',
+      'Parken auf Behindertenparkplatz',
+      'Parken auf Sperrfläche',
+      'Parken auf Busspur',
+      'Fahrzeug blockiert Feuerwehrzufahrt',
+      'Fahrzeug blockiert Elektroladesäule',
       'Parken vor Grundstücksein- und -ausfahrten',
-      'Parken auf schmalen Straßen auch gegenüber von Grundstücksein- und -ausfahrten',
       'Parken vor abgesenkten Bordsteinen',
       'Parken vor und hinter Bahnübergängen mit Andreaskreuz (innerorts besteht im 5-Meter-Bereich, außerorts im 50-Meter-Bereich das Parkverbot)',
       'Parken an Haltestellen (Verkehrszeichen 224) sowie 15 Meter davor und dahinter',
       'Parken im Kreisverkehr',
       'Parken in Fußgängerzonen',
       'Parken im Bereich von Verkehrs- bzw. Durchfahrtsverboten (Verkehrszeichen 250, 252, 253)',
+      'Parken auf schmalen Straßen, auch gegenüber von Grundstücksein- und -ausfahrten',
       'Parken auf Straßen mit durchgezogener Linie',
       'Parken an Engstellen, wenn weniger als 3 Meter zwischen dem parkenden Fahrzeug und der Fahrstreifenbegrenzung verbleiben würden',
-      'Parken auf dem Fußgängerüberweg sowie 5 Meter davor und dahinter',
       'Parken im verkehrsberuhigten Bereich außerhalb der ausgewiesenen Parkflächen',
-      'Parken auf Fahrradschutzstreifen',
     ]
   end
 end
