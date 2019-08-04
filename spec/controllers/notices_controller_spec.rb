3b746de42c3b3386dc116404c25a01f488a70cdc
phoet <phoetmail@googlemail.com>
2019-08-04 19:36:55 +0200

    remove tz test

--- spec/controllers/notices_controller_spec.rb
+++ spec/controllers/notices_controller_spec.rb
@@ -3,25 +3,6 @@ require 'spec_helper'
 describe NoticesController do
   let(:user) { Fabricate(:user) }
 
-  context "time-zone" do
-    render_views
-
-    let(:tz_user) { Fabricate(:user, time_zone: 'Hawaii') }
-
-    before do
-      travel_to(Time.utc(2015, 11, 11, 11, 11, 0)) do
-        @notice = Fabricate(:notice, user: tz_user)
-      end
-
-      login(tz_user)
-    end
-
-    it "should switch to user-tz" do
-      get :show, params: {id: @notice}
-      expect(response.body).to include('11.11. 01:11')
-    end
-  end
-
   context "create" do
     let(:params) {
       {
