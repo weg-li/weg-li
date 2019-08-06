6b70604c43aa6007f6bb899629ceeb70dac0c0e8
phoet <phoetmail@googlemail.com>
2019-08-06 22:26:31 +0200

    use datetimepicker cause firefox sucks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,17 @@
 //= require jquery
 //= require jquery_ujs
-//= require selectize
 //= require turbolinks
 //= require activestorage
 //= require bootstrap/dist/js/bootstrap
+//= require selectize
+//= require moment
+//= require moment/de
+//= require bootstrap-datetimepicker
 //= require custom
 //= require direct_uploads
 //= require google_maps
 //= require styleguide
 //= require_directory ./vues/
 //= require analytics
+
+moment.locale('de');
--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -6,4 +6,5 @@ $(document).on('ready page:load page:change turbolinks:load', function() {
   $('[data-toggle="popover"]').popover();
   $('[data-toggle="tooltip"]').tooltip();
   $('select').selectize();
+  $('.datetimepicker').datetimepicker({locale: 'de', parseInputDate: (value) => { return moment(new Date(value)) }});
 });
