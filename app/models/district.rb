bbfc48a3b75efc055ca237b3c898971420062334
phoet <phoetmail@googlemail.com>
2019-08-10 22:43:37 +0200

    add düsseldorf

--- app/models/district.rb
+++ app/models/district.rb
@@ -2,6 +2,7 @@ class District < Struct.new(:name, :email, :zoom, :latitude, :longitude)
   BERLIN = District.new('berlin', 'anzeige@bowi.berlin.de', 12, 52.520, 13.401)
   BREMEN = District.new('bremen', 'bussgeldstelle@ordnungsamt.bremen.de', 13, 53.078, 8.797)
   DORTMUND = District.new('dortmund', 'fremdanzeigen.verkehrsueberwachung@stadtdo.de', 13, 51.513, 7.460)
+  DUESSELDORF = District.new('duesseldorf', 'bussgeldstelle@duesseldorf.de', 13, 51.219, 6.778)
   FRANKFURT = District.new('frankfurt', 'owi.datenerfassung.amt32@stadt-frankfurt.de', 12, 50.109, 8.675)
   HAMBURG = District.new('hamburg', 'anzeigenbussgeldstelle@eza.hamburg.de', 12, 53.56544, 9.95947)
   KIEL = District.new('kiel', 'ad-bussgeldstelle@kiel.de', 13, 54.319, 10.118)
@@ -12,6 +13,7 @@ class District < Struct.new(:name, :email, :zoom, :latitude, :longitude)
     BERLIN,
     BREMEN,
     DORTMUND,
+    DUESSELDORF,
     FRANKFURT,
     HAMBURG,
     KIEL,
