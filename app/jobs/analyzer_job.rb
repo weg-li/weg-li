b2f270bf01f34937787e57c50faa5c783d0be142
phoet <phoetmail@googlemail.com>
2019-08-13 15:14:39 +0200

    no autocmplete

--- app/jobs/analyzer_job.rb
+++ app/jobs/analyzer_job.rb
@@ -14,12 +14,6 @@ class AnalyzerJob < ApplicationJob
       end
     end
 
-    # - autcomplete weg bei Nummernschild und Ort
-    # - kennzeichen regex mit 4 zahlen
-    # - farbe gucken wo auto ist
-    # - gucken ob nummernschilder erkannt werden
-    # - gucken ob schrift, z.b volvo auftaucht
-
     notice.reverse_geocode
     notice.status = :open
     notice.save_incomplete!
