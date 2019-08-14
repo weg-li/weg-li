8a71644480ee50328fbea3a2047ab2b8820fdc5e
phoet <phoetmail@googlemail.com>
2019-08-14 15:59:28 +0200

    no locale en

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module BurnNotice
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :de
-    config.i18n.available_locales = [:de, :en]
+    config.i18n.available_locales = [:de]
 
     config.generators do |g|
       g.test_framework  :rspec, fixture: false
