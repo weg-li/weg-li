18c4a21221bdf2c4f0e075de1eeeb79eaa922e11
phoet <phoetmail@googlemail.com>
2019-08-14 17:05:33 +0200

    select2 sucks

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,9 +2,14 @@ document.copyToClipboard = function(hint, text) {
   window.prompt(hint, text);
 }
 
-$(document).on('ready page:load page:change turbolinks:load', function() {
+$(document).on('turbolinks:load', function() {
   $('[data-toggle="popover"]').popover();
   $('[data-toggle="tooltip"]').tooltip();
   $('select').select2({ theme: 'bootstrap' });
   $('input[type="file"]').fileinput({ language: 'de', maxFileCount: 5, browseOnZoneClick: true });
+});
+
+// destroy select2 before caching
+$(document).on("turbolinks:before-cache", function () {
+  $('.select2-hidden-accessible').select2('destroy');
 });
