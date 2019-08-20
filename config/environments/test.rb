66a822b708d70b05582a4e033b75633ee87868c2
phoet <phoetmail@googlemail.com>
2019-08-20 12:31:18 +0200

    better raising on missing translations

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -44,4 +44,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = true
+  config.i18n.exception_handler = Proc.new { |exception| raise exception.to_exception }
 end
