9a3b59f07a7e1a2d079f7925783c282f1a6d3947
phoet <phoetmail@googlemail.com>
2019-08-22 20:58:31 +0200

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,7 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -36,7 +37,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
   config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger.
