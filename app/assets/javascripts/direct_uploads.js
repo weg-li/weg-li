6a71f8b036d3deea157670bffca9c0b2c7a8a63d
phoet <phoetmail@googlemail.com>
2019-08-23 09:16:08 +0200

    tweak upload bar

--- app/assets/javascripts/direct_uploads.js
+++ app/assets/javascripts/direct_uploads.js
@@ -3,7 +3,7 @@ addEventListener("direct-upload:initialize", event => {
   const { id, file } = detail;
   target.insertAdjacentHTML("beforebegin", `
     <p>${file.name} (${(file.size / 1048576).toFixed(2)} MB)</p>
-    <div id="direct-upload-${id}" class="progress progress-striped">
+    <div id="direct-upload-${id}" class="progress">
       <div id="direct-upload-progress-${id}" class="progress-bar progress-bar-success" style="width: 0%"></div>
     </div>
   `);
@@ -12,7 +12,7 @@ addEventListener("direct-upload:initialize", event => {
 addEventListener("direct-upload:start", event => {
   const { id } = event.detail;
   const element = document.getElementById(`direct-upload-${id}`);
-  element.classList.add("active");
+  element.classList.add("progress-striped");
 });
 
 addEventListener("direct-upload:progress", event => {
@@ -32,5 +32,5 @@ addEventListener("direct-upload:error", event => {
 addEventListener("direct-upload:end", event => {
   const { id } = event.detail;
   const element = document.getElementById(`direct-upload-${id}`);
-  element.classList.remove("active");
+  element.classList.add("active");
 });
