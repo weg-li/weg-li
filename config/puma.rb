0fd717718b3af74c0385532c99c5fe4bdf405dd8
phoet <phoetmail@googlemail.com>
2019-08-30 15:22:20 +0200

    max 20 connections

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,7 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 4 }
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
@@ -22,6 +22,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # processes).
 #
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+# 20 pg connections on hobby and dev. 2 dynos * 2 workers * 4 threads = 16 + 4 sidekiq (-c 4)
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
