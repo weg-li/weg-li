4d78d5a33934c940c556846ed0f4797b28dec4cc
phoet <phoetmail@googlemail.com>
2019-09-08 21:45:35 +0200

    fix merge conflict

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,7 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 4 }
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 4 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
