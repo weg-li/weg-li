2d2b7b7780d1600cad4865a0cb54b9b3b1e7bc5f
phoet <phoetmail@googlemail.com>
2019-09-24 16:42:37 +0200

    improve geocoding

--- app/models/district.rb
+++ app/models/district.rb
@@ -1,12 +1,16 @@
 class District < ActiveRecord::Base
   validates :name, :zip, :email, presence: :true
 
-  geocoded_by :zip
+  geocoded_by :geocode_address
   after_validation :geocode
 
   def self.legacy_by_zip(zip)
     district = find_by(zip: zip)
     DistrictLegacy.new(district.name, district.name.parameterize, district.email, district.zip, district.latitude, district.longitude)
+  end
+
+  def geocode_address
+    "#{zip}, #{name}, Deutschland"
   end
 
   def display_name
