8847244b83e8fb81b755220dd6a6400120450f1c
phoet <phoetmail@googlemail.com>
2019-09-27 14:20:08 +0200

    make the map much nicer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
     # TODO search for district
     @district = DistrictLegacy.by_name(params[:district] || current_user&.district_name || 'hamburg') || DistrictLegacy::HAMBURG
 
-    @notices = Notice.for_public.since(@since.days.ago).where(district: @district.name)
+    @notices = Notice.shared.since(@since.days.ago).where(district: @district.name)
     @active = @notices.map(&:user_id).uniq.size
     @total = User.count
     @count = User.group(:district).count
