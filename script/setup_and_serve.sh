460bec3abf513862980b39f84a3b0bb621765b39
Marlon Schultz <spam@marlonschultz.de>
2019-11-06 17:58:41 +0100

    removed networks from docker-compose; added volume instead of dockerfile

--- script/setup_and_serve.sh
+++ script/setup_and_serve.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-/var/someRubyFolder/script/setup
-/var/someRubyFolder/script/server
+cd /weg-li || exit
+gem install bundler
+bundle install
+
+sed -i s/localhost/"${DATABASE}"/g /weg-li/config/database.yml
+/weg-li/script/setup
+/weg-li/script/server
