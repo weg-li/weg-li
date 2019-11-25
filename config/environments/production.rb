76a677ec38e9f080db28b9085470b7db21c975f6
phoet <phoetmail@googlemail.com>
2019-11-25 17:48:30 +0100

    all in default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Prepare the ingress controller used to receive mail
   config.action_mailbox.ingress = :sendgrid
+  config.action_mailbox.queues = {
+    incineration: 'default',
+    routing: 'default',
+  }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
