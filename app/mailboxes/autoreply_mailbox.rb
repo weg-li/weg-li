ce26221ce9ecf8300ea7acd6329956df64926a44
phoet <phoetmail@googlemail.com>
2019-11-25 18:32:58 +0100

    fix ingres

--- app/mailboxes/autoreply_mailbox.rb
+++ app/mailboxes/autoreply_mailbox.rb
@@ -1,6 +1,6 @@
 class AutoreplyMailbox < ApplicationMailbox
   def process
     notice = Notice.from_email_address(mail.to.first)
-    notice.replies.create! sender: mail.from, subject: mail.subject, content: mail.body
+    notice.replies.create! sender: mail.from.first, subject: mail.subject, content: mail.body.encoded
   end
 end
