71dcf71e3c2717455a2314523074901548a498f2
phoet <phoetmail@googlemail.com>
2019-11-25 20:10:20 +0100

    make sure to parse the source correctly

--- app/mailboxes/autoreply_mailbox.rb
+++ app/mailboxes/autoreply_mailbox.rb
@@ -1,6 +1,14 @@
 class AutoreplyMailbox < ApplicationMailbox
   def process
     notice = Notice.from_email_address(mail.to.first)
-    notice.replies.create! sender: mail.from.first, subject: mail.subject, content: mail.text_part || mail.html_part || mail.body.encoded
+    notice.replies.create! sender: mail.from.first, subject: mail.subject, content: self.class.content_from_mail(mail)
+  end
+
+  def self.content_from_mail(mail)
+    if mail.multipart?
+      mail.text_part.decoded
+    else
+      mail.decoded
+    end
   end
 end
