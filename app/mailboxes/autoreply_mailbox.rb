1d8f0bf2b10adf7afb1fb82ad99efb0afc52d144
phoet <phoetmail@googlemail.com>
2019-11-26 10:01:28 +0100

    remove testing stuff

--- app/mailboxes/autoreply_mailbox.rb
+++ app/mailboxes/autoreply_mailbox.rb
@@ -4,7 +4,7 @@ class AutoreplyMailbox < ApplicationMailbox
     reply = notice.replies.create! sender: mail.from.first, subject: mail.subject, content: self.class.content_from_mail(mail)
 
     user = notice.user
-    if !user.disable_autoreply_notifications || user.admin?
+    if !user.disable_autoreply_notifications
       UserMailer.autoreply(user, reply).deliver_later
     end
   end
