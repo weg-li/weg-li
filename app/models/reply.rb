c08d5fa17884d2ac367b6e6b2fe2e158475925ad
phoet <phoetmail@googlemail.com>
2019-11-26 20:27:24 +0100

    inbound is optional

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,6 +1,6 @@
 class Reply < ApplicationRecord
   belongs_to :notice
-  belongs_to :action_mailbox_inbound_email, class_name: 'ActionMailbox::InboundEmail'
+  belongs_to :action_mailbox_inbound_email, class_name: 'ActionMailbox::InboundEmail', optional: true
 
   validates :sender, :subject, :content, presence: true
 end
