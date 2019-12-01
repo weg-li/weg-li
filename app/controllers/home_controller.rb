754296cfd9336c72841cb3d55f19706623fe2133
phoet <phoetmail@googlemail.com>
2019-12-01 20:03:04 +0100

    hacking statistics

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,7 +18,7 @@ class HomeController < ApplicationController
     @user_stats = User.count_by_month(User.active, months: @months)
     @active_user_stats = User.count_by_month(User.active.joins(:notices), months: @months)
     @notice_stats = Notice.count_by_month(Notice.shared, months: @months)
-    @photo_stats = Notice.count_by_month(Notice.left_joins(:photos_attachments), months: @months)
+    @photo_stats = Notice.count_by_month(ActiveStorage::Attachment.where(record_type: 'Notice', name: 'photos'), months: @months)
   end
 
   def faq
--- app/models/concerns/statisticable.rb
+++ app/models/concerns/statisticable.rb
@@ -6,10 +6,10 @@ module Statisticable
   class_methods do
     def count_by_month(included_scope = nil, months: 12)
       included_scope ||= self
-      sql = "SELECT d, count(#{table_name}.id) AS c
+      sql = "SELECT d, count(#{included_scope.table_name}.id) AS c
              FROM generate_series( '#{Time.now.utc}'::timestamp - interval '#{months} months', '#{Time.now.utc}'::timestamp, '1 month') d
-             LEFT JOIN #{table_name} ON #{table_name}.created_at <= d
-               AND (#{table_name}.id IN(#{included_scope.select(:id).to_sql}))
+             LEFT JOIN #{included_scope.table_name} ON #{included_scope.table_name}.created_at <= d
+               AND (#{included_scope.table_name}.id IN(#{included_scope.select(:id).to_sql}))
              GROUP BY 1 ORDER BY d;"
 
       result = find_by_sql(sql).pluck(:d, :c)
