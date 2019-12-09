f7eef21c4c952a86db71a3672a21e38e24998846
phoet <phoetmail@googlemail.com>
2019-12-09 15:40:47 +0100

    ignore

--- db/migrate/20141222164002_use_language_for_articles.rb
+++ db/migrate/20141222164002_use_language_for_articles.rb
@@ -3,8 +3,8 @@ class UseLanguageForArticles < ActiveRecord::Migration[4.2]
     add_column :articles, :content, :json
     add_column :articles, :headline, :json
 
-    Article.all.each do |article|
-      article.update! content: {en: article[:body], en: article[:body]}, headline: {en: article[:title], en: article[:title]}
-    end
+    # Article.all.each do |article|
+    #   article.update! content: {en: article[:body], en: article[:body]}, headline: {en: article[:title], en: article[:title]}
+    # end
   end
 end
