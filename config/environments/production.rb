1c94181fd7d4a919d8b7871507876c82de4962f8
phoet <phoetmail@googlemail.com>
2019-12-09 22:31:02 +0100

    use warn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :info
+  config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'warn').to_sym
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :request_id ]
