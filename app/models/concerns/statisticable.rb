5102bb3a3ea444a93c3dbda1e099e17ebce442c0
phoet <phoetmail@googlemail.com>
2019-12-09 16:10:33 +0100

    bi-weekly

--- app/models/concerns/statisticable.rb
+++ app/models/concerns/statisticable.rb
@@ -7,7 +7,7 @@ module Statisticable
     def count_by_month(included_scope = nil, months: 12)
       included_scope ||= self
       sql = "SELECT d, count(#{included_scope.table_name}.id) AS c
-             FROM generate_series('#{Time.now.utc}'::timestamp - interval '#{months} months', '#{Time.now.utc}'::timestamp, '1 month') d
+             FROM generate_series('#{Time.now.utc}'::timestamp - interval '#{months} months', '#{Time.now.utc}'::timestamp, '2 weeks') d
              LEFT JOIN #{included_scope.table_name} ON #{included_scope.table_name}.created_at <= d AND (#{included_scope.table_name}.id IN(#{included_scope.select(:id).to_sql}))
              GROUP BY 1 ORDER BY d;"
 
@@ -18,7 +18,7 @@ module Statisticable
     def sum_by_month(included_scope = nil, months: 12)
       included_scope ||= self
       sql = "SELECT d, count(#{included_scope.table_name}.id) AS c
-             FROM generate_series('#{Time.now.utc}'::timestamp - interval '#{months} months', '#{Time.now.utc}'::timestamp, '1 month') d
+             FROM generate_series('#{Time.now.utc}'::timestamp - interval '#{months} months', '#{Time.now.utc}'::timestamp, '2 weeks') d
              LEFT JOIN #{included_scope.table_name} ON #{included_scope.table_name}.created_at BETWEEN d AND d + interval '1 month'  AND (#{included_scope.table_name}.id IN(#{included_scope.select(:id).to_sql}))
              GROUP BY 1 ORDER BY d;"
 
