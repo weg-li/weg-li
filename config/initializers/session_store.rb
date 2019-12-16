334aa8c440397d0c371a102e45391a90e077ae0e
phoet <phoetmail@googlemail.com>
2019-12-16 09:49:34 +0100

    wip

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_weg_li_session'
+Rails.application.config.session_store :redis_store,
+  servers: [ENV['REDIS_URL']],
+  expire_after: 1.month,
+  key: "_fick_session",
+  threadsafe: true,
+  signed: true,
+  secure: true
