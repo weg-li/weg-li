0b481eafcd2da546fed49f93959737bd749f8989
phoet <phoetmail@googlemail.com>
2019-12-19 13:44:39 +0100

    up until now

--- app/models/concerns/statisticable.rb
+++ app/models/concerns/statisticable.rb
@@ -7,7 +7,7 @@ module Statisticable
     def count_by_month(included_scope = nil, months: 12)
       included_scope ||= self
       sql = "SELECT d, count(#{included_scope.table_name}.id) AS c
-             FROM generate_series('#{Date.today.beginning_of_month}'::timestamp - interval '#{months} months', '#{Date.today.end_of_month}'::timestamp, '2 weeks') d
+             FROM generate_series('#{Date.today.beginning_of_month}'::timestamp - interval '#{months} months', '#{Date.today.end_of_day}'::timestamp, '2 weeks') d
              LEFT JOIN #{included_scope.table_name} ON #{included_scope.table_name}.created_at <= d AND (#{included_scope.table_name}.id IN(#{included_scope.select(:id).to_sql}))
              GROUP BY 1 ORDER BY d;"
 
@@ -18,7 +18,7 @@ module Statisticable
     def sum_by_month(included_scope = nil, months: 12)
       included_scope ||= self
       sql = "SELECT d, count(#{included_scope.table_name}.id) AS c
-             FROM generate_series('#{Date.today.beginning_of_month}'::timestamp - interval '#{months} months', '#{Date.today.end_of_month}'::timestamp, '2 weeks') d
+             FROM generate_series('#{Date.today.beginning_of_month}'::timestamp - interval '#{months} months', '#{Date.today.end_of_day}'::timestamp, '2 weeks') d
              LEFT JOIN #{included_scope.table_name} ON #{included_scope.table_name}.created_at BETWEEN d AND d + interval '2 weeks'  AND (#{included_scope.table_name}.id IN(#{included_scope.select(:id).to_sql}))
              GROUP BY 1 ORDER BY d;"
 
