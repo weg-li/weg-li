1738cd92e563172139da34e67ca466769c6b817d
phoet <phoetmail@googlemail.com>
2020-01-09 10:42:01 +0100

    make previews larger

--- app/helpers/photo_helper.rb
+++ app/helpers/photo_helper.rb
@@ -1,16 +1,14 @@
 module PhotoHelper
   CONFIG = {
     default: {resize: "1280x1280", quality: '90', auto_orient: true},
-    thumb: {resize: "100x100", auto_orient: true},
+    preview: {resize: "200x200", quality: '90', auto_orient: true},
+    thumb: {resize: "100x100", quality: '90', auto_orient: true},
   }
   def url_for_photo(photo, size: :default)
-    case size
-    when :thumb, :default
-      url_for(photo.variant(CONFIG[size]))
-    when :original
+    if size == :original
       url_for(photo)
     else
-      raise "unsuppored size #{size}"
+      url_for(photo.variant(CONFIG[size]))
     end
   end
 end
