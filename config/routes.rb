e6b1faa740e09ed79e1541f9611ceb61503deb5c
phoet <phoetmail@googlemail.com>
2020-01-17 15:00:39 +0100

    properly map the errrors

--- config/routes.rb
+++ config/routes.rb
@@ -105,5 +105,8 @@ Rails.application.routes.draw do
   get '/styleguide', to: 'styleguide#index'
 
   get '/404', to: "errors#not_found"
+  get '/422', to: "errors#unacceptable"
+  get '/500', to: "errors#internal_error"
+
   get '/ping', to: -> (env) { [200, {'Content-Type' => 'text/html'}, ['pong']] }
 end
