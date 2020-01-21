4d58264d28931943edf435784c7055816d984707
phoet <phoetmail@googlemail.com>
2020-01-21 16:15:09 +0100

    fix the renderer settings

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,8 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
+ActiveSupport::Reloader.to_prepare do
+  if Rails.env.production?
+    ApplicationController.renderer.defaults.merge!(
+      http_host: 'www.weg-li.de',
+      https: true
+    )
+  else
+    ApplicationController.renderer.defaults.merge!(
+      http_host: 'example.org',
+      https: false
+    )
+  end
+end
