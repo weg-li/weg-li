0ecbcf5e3cfef3f31661078cd987fdf9bf7221d0
phoet <phoetmail@googlemail.com>
2020-01-22 11:05:32 +0100

    fixate time

--- spec/lib/pdf_generator_spec.rb
+++ spec/lib/pdf_generator_spec.rb
@@ -4,13 +4,15 @@ describe PDFGenerator, :vcr do
   let(:example) { File.binread(file_fixture('anzeige.pdf')) }
 
   it "handles the pdf generation" do
-    travel_to('01.01.2020'.to_date) do
+    travel_to('20.01.2020 15:00:00 UTC'.to_time.utc) do
       district = Fabricate(:district, zip: '12345')
       user = Fabricate.build(:user, name: 'Uschi Müller', email: 'test@example.com', city: 'Dorf', zip: '12345', street: 'Am Weiher 123')
       notice = Fabricate.build(:notice, user: user, charge: 'doof parken', brand: 'märzer', color: 'black', registration: 'HH AB 123', city: 'Dorf', street: 'Am Weiher 123', zip: '12345', district: district, token: 'xxxxxxx')
       notice.save!
 
       result = PDFGenerator.new.generate(notice)
+
+      # File.open('example.pdf', 'wb') {|f| f.write(result)}
 
       expect(example).to eql(result)
     end
