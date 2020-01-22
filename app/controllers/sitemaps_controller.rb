72d59ea120f7f947d7c215051637a9236af9864b
phoet <phoetmail@googlemail.com>
2020-01-22 15:00:43 +0100

    add some urls

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -4,8 +4,13 @@ class SitemapsController < ApplicationController
       root_url,
       faq_url,
       map_url,
+      stats_url,
+      districts_url,
+      privacy_url,
+      faq_url,
+      year2019_url,
+      year2020_url,
     ]
-
 
     respond_to do |format|
       format.xml { render xml: @urls }
