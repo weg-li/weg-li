ba87018402aebc858b6e231bf1a38a741621c413
phoet <phoetmail@googlemail.com>
2020-02-12 13:34:41 +0100

    more time for reminding of usage

--- app/jobs/scheduled/usage_reminder_job.rb
+++ app/jobs/scheduled/usage_reminder_job.rb
@@ -2,9 +2,9 @@ class Scheduled::UsageReminderJob < ApplicationJob
   def perform
     Rails.logger.info "deaktivate old users"
 
-    not_using = User.active.left_joins(:notices).where('notices.id IS NULL').where("date_part('dow', users.updated_at) = ?", Date.today.wday).where('users.updated_at < ?', 1.month.ago)
+    not_using = User.active.left_joins(:notices).where('notices.id IS NULL').where("date_part('dow', users.updated_at) = ?", Date.today.wday).where('users.updated_at < ?', 2.month.ago)
     not_using.each do |user|
-      if user.updated_at < 2.month.ago
+      if user.updated_at < 3.month.ago
         Rails.logger.info "deactivating inactive user #{user.id} #{user.name} #{user.email}"
         user.update! access: :disabled
       else
