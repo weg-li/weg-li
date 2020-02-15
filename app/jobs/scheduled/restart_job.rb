b724ef98503e1ea943cb3fec71095d2a62afb412
phoet <phoetmail@googlemail.com>
2020-02-15 12:37:12 +0100

    fix the test

--- app/jobs/scheduled/restart_job.rb
+++ app/jobs/scheduled/restart_job.rb
@@ -1,12 +1,12 @@
 class Scheduled::RestartJob < ApplicationJob
   def perform
     Rails.logger.info("reschedule aborted processors")
-    Notice.analyzing.where('updated_at > ?', 5.minutes.ago).each do |notice|
+    Notice.analyzing.where('updated_at < ?', 2.minutes.ago).each do |notice|
       Rails.logger.info "restarting notice #{notice.id}"
       notice.analyze!
     end
 
-    BulkUpload.processing.where('updated_at > ?', 15.minutes.ago).each do |bulk_upload|
+    BulkUpload.processing.where('updated_at < ?', 5.minutes.ago).each do |bulk_upload|
       Rails.logger.info "restarting bulk #{bulk_upload.id}"
       bulk_upload.analyze!
     end
--- spec/jobs/scheduled/restart_job_spec.rb
+++ spec/jobs/scheduled/restart_job_spec.rb
@@ -3,8 +3,11 @@ require 'spec_helper'
 describe Scheduled::RestartJob do
   context "perform" do
     it "should process the bulk-upload" do
-      Fabricate.create(:notice, status: :analyzing, updated_at: 1.hour.ago)
-      Fabricate.create(:bulk_upload, status: :processing, updated_at: 1.hour.ago)
+      notice = Fabricate.create(:notice, status: :analyzing)
+      bulk_upload = Fabricate.create(:bulk_upload, status: :processing)
+
+      notice.update_attribute(:updated_at, 5.minutes.ago)
+      bulk_upload.update_attribute(:updated_at, 10.minutes.ago)
 
       expect {
         expect {
