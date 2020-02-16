e98670e7c9e6d7b69ced2f6f5e2037e2eaa0cca8
phoet <phoetmail@googlemail.com>
2020-02-16 11:17:35 +0100

    add json to endpoint

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,6 +2,11 @@ class PublicController < ApplicationController
   def charge
     @notice = Notice.for_public.find_by(token: params[:token])
     _404 and return if @notice.blank?
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @notice.as_api_response(:public_beta) }
+    end
   end
 
   def profile
