f415ce6aa4846558eb82bbdea3104b610fb47526
phoet <phoetmail@googlemail.com>
2020-03-07 15:22:59 +0100

    auto cleanup missing geocoding

--- app/jobs/scheduled/geocoding_cleanup_job.rb
+++ app/jobs/scheduled/geocoding_cleanup_job.rb
@@ -2,6 +2,10 @@ class Scheduled::GeocodingCleanupJob < ApplicationJob
   def perform
     Rails.logger.info "fixup for geocoding"
 
+    Notice.shared.where(latitude: nil).each do |notice|
+      notice.save!
+    end
+
     query = "
     select * from (
     select notices.id, 2 * 3961 * asin(sqrt((sin(radians((districts.latitude - notices.latitude) / 2))) ^ 2 + cos(radians(notices.latitude)) * cos(radians(districts.latitude)) * (sin(radians((districts.longitude - notices.longitude) / 2))) ^ 2)) as distance from notices join districts on notices.zip = districts.zip where notices.incomplete = FALSE and notices.latitude IS NOT NULL and districts.latitude IS NOT NULL
