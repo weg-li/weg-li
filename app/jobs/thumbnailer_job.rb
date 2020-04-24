0fb42ad66b6e7bc45a5916d60e1b03c86be27aae
phoet <phoetmail@googlemail.com>
2020-04-24 13:10:53 +0200

    discard invariable images

--- app/jobs/thumbnailer_job.rb
+++ app/jobs/thumbnailer_job.rb
@@ -1,6 +1,7 @@
 class ThumbnailerJob < ApplicationJob
   retry_on ActiveStorage::FileNotFoundError, attempts: 15, wait: :exponentially_longer
   retry_on MiniMagick::Error, attempts: 15, wait: :exponentially_longer
+  discard_on ActiveStorage::InvariableError
 
   def perform(blob)
     Rails.logger.info("analyzing #{blob.filename}")
