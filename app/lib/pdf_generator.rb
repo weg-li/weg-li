b1f0368a1ffe80b34d7ac38b1ebb408eb19b92e4
Peter Schröder <peter.schroder@shopify.com>
2020-08-26 14:29:14 +0200

    Split PDF text
    closes #256

--- app/lib/pdf_generator.rb
+++ app/lib/pdf_generator.rb
@@ -4,9 +4,17 @@ require 'prawn/qrcode'
 class PDFGenerator
   def generate(notice, quality: :default)
     user = notice.user
-    content = renderer.render(
-      template: '/notice_mailer/pdf.text.erb',
-      locals: { :@notice => notice, :@user => user }
+    header = renderer.render(
+      template: '/notice_mailer/_header.text.erb',
+      locals: { :notice => notice, :user => user }
+    )
+    details = renderer.render(
+      template: '/notice_mailer/_details.text.erb',
+      locals: { :notice => notice, :user => user }
+    )
+    footer = renderer.render(
+      template: '/notice_mailer/_footer.text.erb',
+      locals: { :notice => notice, :user => user }
     )
 
     pdf = Prawn::Document.new do |document|
@@ -15,21 +23,35 @@ class PDFGenerator
 
       document.move_cursor_to(document.bounds.height)
       document.font_size(10)
-      document.text(content)
+      document.text(header)
+      document.move_down(20)
+      document.text(details)
+
+
+      document.start_new_page
+      document.text(footer)
+      document.move_down(20)
 
       if user.signature.present?
-        document.move_cursor_to(100)
         user.signature.service.download_file(user.signature.key) { |file| document.image(file, fit: [300, 50]) }
+      else
+        document.move_down(50)
+
       end
-      document.move_cursor_to(50)
-      document.font_size(10)
+      document.render_qr_code(qr_code, pos: [document.bounds.width - 50, document.cursor])
       document.text("_" * 40)
       document.text("#{user.city}, #{I18n.l(Date.today)}")
+
+
+      document.start_new_page
 
       notice.photos.each do |photo|
         variant = quality == :original ? photo : photo.variant(PhotoHelper::CONFIG[quality]).processed
         photo.service.download_file(variant.key) { |file| document.image(file, fit: [document.bounds.width, document.bounds.height / 2]) }
       end
+
+      document.font_size(8)
+      document.number_pages "Seite <page> von <total>", at: [document.bounds.width - 50, -15]
     end
     pdf.render
   end
