d884f3934c9a3cbdc03b290a4672975ec78db621
Peter Schröder <phoetmail@googlemail.com>
2020-11-06 17:42:13 +0100

    check it out

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -7,23 +7,24 @@ ActiveStorage::DirectUploadsController.instance_eval do
   rescue_from(ActionController::InvalidAuthenticityToken, with: lambda { redirect_to('/', alert: 'Deine Sitzung wurde unerwartet beendet!') })
 end
 
-ActiveStorage::RepresentationsController.instance_eval do
-  rescue_from(
-    MiniMagick::Error,
-    with: lambda {
-      response.set_header('Retry-After', 2)
-      redirect_to(request.url, status: 302)
-    }
-  )
-  rescue_from(
-    ActiveStorage::FileNotFoundError,
-    with: lambda {
-      response.set_header('Retry-After', 2)
-      redirect_to(request.url, status: 302)
-    }
-  )
-  rescue_from(ActiveRecord::RecordNotFound, with: lambda { head(404) })
-end
+# wait and see where this will need to go
+# ActiveStorage::RepresentationsController.instance_eval do
+#   rescue_from(
+#     MiniMagick::Error,
+#     with: lambda {
+#       response.set_header('Retry-After', 2)
+#       redirect_to(request.url, status: 302)
+#     }
+#   )
+#   rescue_from(
+#     ActiveStorage::FileNotFoundError,
+#     with: lambda {
+#       response.set_header('Retry-After', 2)
+#       redirect_to(request.url, status: 302)
+#     }
+#   )
+#   rescue_from(ActiveRecord::RecordNotFound, with: lambda { head(404) })
+# end
 
 require 'active_storage/service/gcs_service'
 require 'active_storage/service/disk_service'
@@ -54,8 +55,10 @@ ActiveSupport::Reloader.to_prepare do
 
   class ActiveStorage::Blob < ActiveRecord::Base
     before_validation :set_usable_key_not_the_shait_from_active_storate
+
     def set_usable_key_not_the_shait_from_active_storate
-      self[:key] ||= "#{SecureRandom.base36(28)}#{File.extname(self[:filename])}" if self[:filename].present?
+      # REM: add a file-extension to the key .jpg
+      self[:key] ||= "#{self.class.generate_unique_secure_token(length: MINIMUM_TOKEN_LENGTH)}#{File.extname(self[:filename])}" if self[:filename].present?
     end
   end
 end
