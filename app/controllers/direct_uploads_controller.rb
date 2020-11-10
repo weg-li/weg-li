5d7d62044c3ff836ca049855435602559653a8b9
Peter Schröder <phoetmail@googlemail.com>
2020-11-10 10:43:29 +0100

    use changed method

--- app/controllers/direct_uploads_controller.rb
+++ app/controllers/direct_uploads_controller.rb
@@ -2,7 +2,8 @@ class DirectUploadsController < ActionController::Base
   def analyze
     signed_id = params.dig(:blob, :signed_id)
 
-    blob = ActiveStorage::Blob.find_signed(signed_id)
+    blob = ActiveStorage::Blob.find_signed!(signed_id)
+
     ThumbnailerJob.perform_later(blob)
 
     render json: blob.as_json
