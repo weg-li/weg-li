53bbf6ac714d45eebdc268aea4d9fb66fe44065d
Peter Schröder <phoetmail@googlemail.com>
2020-11-10 21:54:11 +0100

    restore option values manually
    fixes #219

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -9,7 +9,20 @@ $(document).on('turbolinks:load', function() {
   $('.zoom').zoom();
 });
 
-// destroy select2 before caching
 $(document).on("turbolinks:before-cache", function () {
+  // destroy select2 before caching
   $('.select2-hidden-accessible').select2('destroy');
+});
+
+$(document).on("submit", function () {
+  // https://github.com/weg-li/weg-li/issues/219
+  // https://github.com/turbolinks/turbolinks/issues/238
+  $('select').each(function () {
+    const el = $(this);
+    const selected = el.val();
+    el.find('option').attr('selected', null);
+    if (selected) {
+      el.find(`option[value="${selected}"]`).attr('selected', true);
+    }
+  });
 });
