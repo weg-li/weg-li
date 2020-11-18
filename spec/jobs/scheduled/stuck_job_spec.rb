69216e5c1293dfca2d897d70d15f53f4afe19dae
Peter Schröder <phoetmail@googlemail.com>
2020-11-18 14:28:06 +0100

    dont run

--- spec/jobs/scheduled/stuck_job_spec.rb
+++ spec/jobs/scheduled/stuck_job_spec.rb
@@ -3,9 +3,7 @@ require 'spec_helper'
 describe Scheduled::StuckJob do
   context "perform" do
     it "should kill the process" do
-      return if ENV['CI']
-
-      Scheduled::StuckJob.perform_now
+      Scheduled::StuckJob.perform_now unless ENV['CI']
     end
   end
 end
