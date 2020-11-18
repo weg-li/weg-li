31d361d1c4a25cd94b18c3efee5c97aa4b4d46e6
Peter Schröder <phoetmail@googlemail.com>
2020-11-18 10:55:36 +0100

    10 seconds per image is more likely

--- app/jobs/bulk_upload_job.rb
+++ app/jobs/bulk_upload_job.rb
@@ -4,7 +4,7 @@ class BulkUploadJob < ApplicationJob
       ThumbnailerJob.perform_later(photo.blob)
     end
 
-    wait = bulk_upload.photos.size.seconds
+    wait = (bulk_upload.photos.size * 10).seconds
     BulkUploadUpdateJob.set(wait: wait).perform_later(bulk_upload)
   end
 end
