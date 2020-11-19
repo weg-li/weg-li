293ee7510fcdc4b39214565cef42bcb706a3e907
Peter Schröder <phoetmail@googlemail.com>
2020-11-19 10:50:01 +0100

    add notifications

--- app/jobs/scheduled/stuck_job.rb
+++ app/jobs/scheduled/stuck_job.rb
@@ -1,6 +1,6 @@
 class Scheduled::StuckJob < ApplicationJob
   def perform
-    Rails.logger.warn("checking for stuck jobs")
+    Rails.logger.debug("checking for stuck jobs")
 
     processes.each do |process|
       id = process.identity
@@ -14,15 +14,17 @@ class Scheduled::StuckJob < ApplicationJob
 
         busy_workers = workers.select { |process, thread, msg| process == id }
         dead_workers = busy_workers.select { |process, thread, msg| Time.at(msg['run_at']) < 2.minutes.ago }
+        dead = dead.size
 
-        if dead_workers.size >= concurrent / 2
-          Rails.logger.warn("process #{id} has #{dead_workers.size} dead jobs, killing it now!")
+
+        if dead >= concurrent / 2
+          notify("process #{id} has #{dead} of #{concurrent} dead jobs, killing it now!")
           Sidekiq::Process.new(id).stop!
         else
-          Rails.logger.debug("process #{id} has just #{dead_workers.size} dead jobs, #{busy_workers.size - dead_workers.size} still busy")
+          notify("process #{id} has just #{dead} of #{concurrent} dead jobs, #{concurrent - dead} still busy")
         end
       else
-        Rails.logger.warn("process #{id} is ok with concurrent #{busy}/#{concurrent}")
+        Rails.logger.debug("process #{id} is ok with #{busy} of #{concurrent}")
       end
     end
   end
