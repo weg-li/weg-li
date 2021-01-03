294fadf6d5e543abb81c372b1617f7294e47dfa0
Peter Schröder <phoetmail@googlemail.com>
2021-01-03 15:59:04 +0100

    remove heroku remainders

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,6 @@ Rails.application.configure do
     port: '587',
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
-    domain: 'heroku.com',
     enable_starttls_auto: true
   }
 
