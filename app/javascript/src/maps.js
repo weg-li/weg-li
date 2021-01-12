d34ffc73a919eafc19a07b7137e29f170c105090
Peter Schröder <phoetmail@googlemail.com>
2021-01-12 21:22:13 +0100

    adapt mapbox tiles
    closes #348

--- app/javascript/src/maps.js
+++ app/javascript/src/maps.js
@@ -36,6 +36,9 @@ function initMap(canvas, coords, zoom = 13) {
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox/outdoors-v11',
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoicGhvZXQiLCJhIjoiY2s4b2Y3cGdqMDIzZDNkbnMwMzhlMnJpbiJ9.n2Fw_hniWAZ8T5-Qc0V0fA',
   }).addTo(map);
   return map;
