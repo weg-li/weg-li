d2c8923ad1cfe4db2c8aa491bc3ffd45e91c1c1e
Peter Schröder <phoetmail@googlemail.com>
2021-01-26 21:42:42 +0100

    remove turbo

--- app/javascript/src/custom.js
+++ app/javascript/src/custom.js
@@ -1,33 +1,15 @@
 require('select2');
 require('jquery-zoom');
 
-$(document).on('turbolinks:load', () => {
+$(document).ready(() => {
   $('[data-toggle="popover"]').popover();
   $('[data-toggle="tooltip"]').tooltip();
 });
 
-$(document).on('turbolinks:load', () => {
+$(document).ready(() => {
   $('.zoom').zoom();
 });
 
-$(document).on('turbolinks:load', () => {
+$(document).ready(() => {
   $('select[data-select2-disabled!="true"]').select2({ theme: 'bootstrap' });
-});
-
-$(document).on('turbolinks:before-cache', () => {
-  // destroy select2 before caching
-  $('.select2-hidden-accessible').select2('destroy');
-});
-
-$(document).on('submit', () => {
-  // https://github.com/weg-li/weg-li/issues/219
-  // https://github.com/turbolinks/turbolinks/issues/238
-  $('select').each(function hack() {
-    const el = $(this);
-    const selected = el.val();
-    el.find('option').attr('selected', null);
-    if (selected) {
-      el.find(`option[value="${selected}"]`).attr('selected', true);
-    }
-  });
 });
