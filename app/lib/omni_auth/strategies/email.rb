78e797209c01986e5853713477af7ddb8aae452d
Peter Schröder <phoetmail@googlemail.com>
2021-02-02 10:06:25 +0100

    next try

--- app/lib/omni_auth/strategies/email.rb
+++ app/lib/omni_auth/strategies/email.rb
@@ -12,18 +12,18 @@ module OmniAuth
       def callback_phase
         token = request.params['token']
         if token.blank?
-          fail!(:authenticity_error, 'token was missing')
+          fail!(:authenticity_error)
         end
 
         begin
           decoded_token = Token.decode(token)
         rescue
-          fail!(:authenticity_error, 'token was tempered with')
+          fail!(:authenticity_error, $!)
         end
 
         @email = decoded_token['iss'].to_s.downcase
         if @email.blank?
-          fail!(:authenticity_error, 'token iss was not an email')
+          fail!(:authenticity_error)
         end
 
         super
