4daec579d215b114b5998035954cc02c592f35c5
Peter Schröder <phoetmail@googlemail.com>
2021-03-12 11:05:13 +0100

    disable bulk downloads

--- spec/jobs/photos_download_job_spec.rb
+++ spec/jobs/photos_download_job_spec.rb
@@ -3,17 +3,17 @@ require 'spec_helper'
 describe PhotosDownloadJob do
   let(:bulk_upload) { Fabricate.create(:bulk_upload, shared_album_url: 'https://photos.app.goo.gl/85akvN1q3xxcT4Hu9') }
 
-  context "perform" do
-    it "download the archive from google photos" do
-      expect { PhotosDownloadJob.perform_now(bulk_upload) }.to change { bulk_upload.photos.count }.by(1)
-    end
-
-    it "handles broken urls" do
-      bulk_upload = Fabricate.create(:bulk_upload, shared_album_url: 'https://photos.app.goo.gl/not_found')
-      expect { PhotosDownloadJob.perform_now(bulk_upload) }.to change { bulk_upload.status }.from('open').to('error')
-
-      bulk_upload = Fabricate.create(:bulk_upload, shared_album_url: 'https://www.weg.li')
-      expect { PhotosDownloadJob.perform_now(bulk_upload) }.to change { bulk_upload.status }.from('open').to('error')
-    end
-  end
+  # context "perform" do
+  #   it "download the archive from google photos" do
+  #     expect { PhotosDownloadJob.perform_now(bulk_upload) }.to change { bulk_upload.photos.count }.by(1)
+  #   end
+  #
+  #   it "handles broken urls" do
+  #     bulk_upload = Fabricate.create(:bulk_upload, shared_album_url: 'https://photos.app.goo.gl/not_found')
+  #     expect { PhotosDownloadJob.perform_now(bulk_upload) }.to change { bulk_upload.status }.from('open').to('error')
+  #
+  #     bulk_upload = Fabricate.create(:bulk_upload, shared_album_url: 'https://www.weg.li')
+  #     expect { PhotosDownloadJob.perform_now(bulk_upload) }.to change { bulk_upload.status }.from('open').to('error')
+  #   end
+  # end
 end
