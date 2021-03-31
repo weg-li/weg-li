bc81d1c5b74c7c04678677e2b8346938a767e82a
Peter Schröder <phoetmail@googlemail.com>
2021-03-31 10:53:02 +0200

    upsi

--- app/jobs/analyzer_job.rb
+++ app/jobs/analyzer_job.rb
@@ -22,7 +22,6 @@ class AnalyzerJob < ApplicationJob
 
     handle_exif(notice)
     handle_vision(notice)
-    handle_car_ml(notice)
 
     notice.save_incomplete!
   end
@@ -75,36 +74,6 @@ class AnalyzerJob < ApplicationJob
     notice.apply_dates(dates)
 
     notice.handle_geocoding
-  end
-
-  def handle_car_ml(notice)
-    api = create_api
-
-    photos = notice.photos.first(3)
-    image_upload_response = api.analyze_image_upload_get(quantity: photos.size)
-    photos.each_with_index do |photo, i|
-      url = image_upload_response.google_cloud_urls[i]
-
-      photo.service.download_file(photo.key) do |file|
-        header = { 'Content-Type' => 'image/jpeg' }
-        res = HTTPClient.new.put(url, header: header, body: file)
-        notify("could not upload photo #{photo.id} for notice: https://www.weg.li/admin/notices/#{notice.token}") unless res.ok?
-      end
-    end
-
-    response = api.analyze_image_image_token_get(image_upload_response.token)
-    notice.data_sets.create!(data: response.to_json, kind: :car_ml, keyable: notice)
-  end
-
-  def create_api
-    config = OpenapiClient::Configuration.new
-    config.host = 'https://europe-west3-wegli-296209.cloudfunctions.net/'
-    config.base_path = 'api'
-  	config.debugging = true
-
-    client = OpenapiClient::AnalyzeApi.new
-    client.api_client.config = config
-    client
   end
 
   def time_from_meta(timestamp)
