8e22c3ad0d4632ac49fbe6269b16f27bed7fa95a
Peter Schröder <phoetmail@googlemail.com>
2021-04-07 21:21:37 +0200

    last try

--- db/migrate/20210407145029_add_postgis.rb
+++ db/migrate/20210407145029_add_postgis.rb
@@ -1,14 +1,9 @@
 class AddPostgis < ActiveRecord::Migration[6.1]
   def change
-    change_table :notices do |t|
-      t.st_point :lonlat, geographic: true
-
-      t.index :lonlat, using: :gist
-    end
-
     reversible do |dir|
       dir.up do
         execute "CREATE EXTENSION IF NOT EXISTS postgis;"
+        execute "CREATE INDEX index_notices_on_lonlat ON notices USING GIST (lonlat);"
         execute "UPDATE notices SET lonlat = ST_MakePoint(longitude, latitude) WHERE lonlat IS NULL AND longitude IS NOT NULL AND latitude IS NOT NULL;"
       end
     end
