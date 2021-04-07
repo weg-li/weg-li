6a1dcb51a733befa3ec043edc29977089caaadb3
Peter Schröder <phoetmail@googlemail.com>
2021-04-07 21:32:01 +0200

    very very last try

--- db/migrate/20210407145029_add_postgis.rb
+++ db/migrate/20210407145029_add_postgis.rb
@@ -5,7 +5,7 @@ class AddPostgis < ActiveRecord::Migration[6.1]
         execute "CREATE EXTENSION IF NOT EXISTS postgis;"
         execute "ALTER TABLE notices ADD COLUMN lonlat geometry(Point, 4326);"
         execute "CREATE INDEX index_notices_on_lonlat ON notices USING GIST (lonlat);"
-        execute "UPDATE notices SET lonlat = ST_MakePoint(longitude, latitude) WHERE lonlat IS NULL AND longitude IS NOT NULL AND latitude IS NOT NULL;"
+        execute "UPDATE notices SET lonlat = ST_SetSRID(ST_MakePoint(longitude, latitude), 4326) WHERE lonlat IS NULL AND longitude IS NOT NULL AND latitude IS NOT NULL;"
       end
     end
   end
