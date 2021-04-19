83edb83c5911f698164f799f61dbcc8f8e9e5c31
Peter Schröder <phoetmail@googlemail.com>
2021-04-19 10:18:06 +0200

    move back into one job

--- app/jobs/analyzer_job.rb
+++ app/jobs/analyzer_job.rb
@@ -20,14 +20,50 @@ class AnalyzerJob < ApplicationJob
   end
 
   def analyze(notice)
-
     handle_exif(notice)
+
+    begin
+      handle_ml(notice)
+    rescue HTTP::TimeoutError => exception
+      Appsignal.set_error(exception)
+      handle_vision(notice)
+    end
 
     notice.status = :open
     notice.save_incomplete!
   end
 
   private
+
+  def handle_ml(notice)
+    notice.photos.each do |photo|
+      result = annotator.annotate_yolo(photo.key)
+
+      if result.present?
+        data_set = notice.data_sets.create!(data: result, kind: :car_ml, keyable: photo)
+        # skip if we got lucky
+        return if data_set.registrations.present?
+      end
+    end
+  end
+
+  def handle_vision(notice)
+    prefixes = notice.user.district&.prefixes || []
+
+    notice.photos.each do |photo|
+      result = annotator.annotate_object(photo.key)
+
+      if result.present?
+        data_set = notice.data_sets.create!(data: result, kind: :google_vision, keyable: photo)
+        # skip if we got lucky
+        return if data_set.registrations.present?
+      end
+    end
+  end
+
+  def annotator
+    @annotator ||= Annotator.new
+  end
 
   def handle_exif(notice)
     notice.photos.each do |photo|
--- spec/jobs/analyzer_job_spec.rb
+++ spec/jobs/analyzer_job_spec.rb
@@ -4,13 +4,35 @@ describe AnalyzerJob do
   context "perform" do
     let(:notice) { Fabricate.create(:notice) }
 
-    it "should analyze the exif data and geocode" do
+    it "should analyze the image with yolo" do
+      job = AnalyzerJob.new
+      stub_request(:post, "https://weg-li-car-ml.onrender.com/").to_return(status: 200, body: "{\"suggestions\":{}}", headers: {})
+
       expect {
-        AnalyzerJob.new.analyze(notice)
+        job.analyze(notice)
       }.to change {
         notice.data_sets.count
-      }.by(2)
+      }.by(3)
     end
+
+    it "should analyze the image with vision if yolo fails" do
+      job = AnalyzerJob.new
+      this = self
+      job.define_singleton_method(:annotator) { this }
+      def job.handle_ml(notice)
+        raise HTTP::TimeoutError
+      end
+
+      expect {
+        job.analyze(notice)
+      }.to change {
+        notice.data_sets.count
+      }.by(3)
+    end
+  end
+
+  def annotate_object(key)
+    with_fixture('annotate') { Annotator.new.annotate_object(key) }
   end
 
   context "helpers" do
