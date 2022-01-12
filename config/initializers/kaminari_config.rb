b3659759b62f3580ffe723286470de0807353165
Peter Schröder <phoet@posteo.de>
2022-01-12 19:02:23 +0100

    more notices 25
    closes #442

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,6 +1,6 @@
 Kaminari.configure do |config|
   # 15 for now, so we can actually load all images at once for the notices/index page
-  config.default_per_page = 15
+  config.default_per_page = 25
   # config.max_per_page = nil
   # config.window = 4
   # config.outer_window = 0
