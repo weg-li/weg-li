928192db00b69fe0c88318c637a8931ee5f3ac56
Peter Schröder <phoet@posteo.de>
2022-05-19 16:31:20 +0200

    remove stuck jobs

--- spec/jobs/scheduled/stuck_job_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Scheduled::StuckJob do
-  context "perform" do
-    it "should kill the process" do
-      Scheduled::StuckJob.perform_now unless ENV['CI']
-    end
-  end
-end
