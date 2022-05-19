49eed347ee1e2cdad059f302f9c3396e5b4a96a8
Peter Schröder <phoet@posteo.de>
2022-05-19 15:04:02 +0200

    remove cdn options

--- app/helpers/photo_helper.rb
+++ app/helpers/photo_helper.rb
@@ -7,36 +7,30 @@ module PhotoHelper
 
   def url_for_photo(photo, size: :default)
     if size == :original
-      rails_storage_redirect_url(photo, cdn_url_options)
+      rails_storage_redirect_url(photo)
     else
       if access?(:community)
         cloudflare_image_resize_url(photo, size)
       else
-        rails_storage_redirect_url(photo.variant(CONFIG[size]), cdn_url_options)
+        rails_storage_redirect_url(photo.variant(CONFIG[size]))
       end
     end
   rescue ActiveStorage::InvariableError => e
     Rails.logger.warn("rendering broken image #{photo.id}: #{e.message}")
     url_for(photo)
   end
-  
+
   def cloudflare_image_resize_url(photo, size)
     width, height = CONFIG[size][:resize].split('x')
     quality = CONFIG[size][:quality]
     "https://images.weg.li/cdn-cgi/image/width=#{width},height=#{height},fit=scale-down,quality=#{quality}/storage/#{photo.key}"
   end
-  
+
   def variant_exists?(photo, size: :default)
     access?(:community) || photo.variant(CONFIG[size]).processed?
   rescue ActiveStorage::InvariableError => e
     Rails.logger.warn("rendering broken image #{photo.id}: #{e.message}")
     false
-  end
-
-  def cdn_url_options
-    cdn_host = ENV['CDN_HOST']
-
-    cdn_host ? { host: cdn_host } : { only_path: true }
   end
 
   def photo_url_with_variant(photo, size: :default, &block)
