2e0885f3670b69fcbcdbbd52c0681aaba57ead89
Peter Schröder <phoet@posteo.de>
2022-05-31 17:50:18 +0200

    remove restart job

--- app/jobs/scheduled/restart_job.rb
+++
@@ -1,9 +0,0 @@
-class Scheduled::RestartJob < ApplicationJob
-  def perform
-    Rails.logger.info("reschedule aborted processors")
-    Notice.analyzing.where('updated_at < ?', 2.minutes.ago).each do |notice|
-      Rails.logger.info "restarting notice #{notice.id}"
-      notice.analyze!
-    end
-  end
-end
