25dc0c0a373eb8644d6e04ec6cd945e75f182a7d
Peter Schröder <phoet@posteo.de>
2022-11-03 22:53:28 +0100

    preload attachments

--- app/controllers/exports_controller.rb
+++ app/controllers/exports_controller.rb
@@ -1,5 +1,5 @@
 class ExportsController < ApplicationController
   def index
-    @exports = Export.for_public.order(params[:order] || 'created_at DESC').page(params[:page])
+    @exports = Export.for_public.order(params[:order] || 'created_at DESC').with_attached_archive.page(params[:page])
   end
 end
