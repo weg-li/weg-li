36e2a07063cc3ebefdf07041d39de4214903652f
Peter Schröder <phoet@posteo.de>
2022-11-08 09:47:41 +0100

    use from render
    https://render.com/docs/deploy-rails#create-a-build-script

--- entrypoints/render-build.sh
+++ entrypoints/render-build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+# exit on error
+set -o errexit
 
 bundle install
-bundle exec rake db:migrate
 bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
