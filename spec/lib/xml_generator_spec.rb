d0a1ce9730f6edebf5594f9c689098ccd4d9b517
Peter Schröder <phoet@posteo.de>
2023-03-06 21:09:22 +0100

    finish annotation

--- spec/lib/xml_generator_spec.rb
+++ spec/lib/xml_generator_spec.rb
@@ -8,14 +8,14 @@ describe XmlGenerator do
   it "handles the xml generation" do
     travel_to("20.01.2020 15:00:00 UTC".to_time.utc) do
       district = Fabricate(:district, zip: "12345")
-      user = Fabricate.build(:user, name: "Uschi Müller", email: "test@example.com", city: "Dorf", zip: "54321", street: "Am Weiher 123")
+      user = Fabricate.build(:user, name: "Uschi Müller", email: "test@example.com", city: "Dorf", zip: "54321", street: "Am Weiher 123", appendix: "2. OG", phone: "0178123456", date_of_birth: "31.12.2000")
       notice = Fabricate.build(:notice, user:, charge: Charge::CHARGES[112_474], brand: "märzer", color: "black", registration: "HH AB 123", city: "Dorf", street: "Am Weiher 123", zip: "12345", district:, token: "xxxxxxx")
       notice.save!
       notice.photos.first.update!(key: "test.jpg")
 
       result = XmlGenerator.new.generate(notice)
 
-      # file_fixture('anzeige.xml').write(result)
+      # file_fixture("anzeige.xml").write(result)
       expect(example).to eql(result)
     end
   end
