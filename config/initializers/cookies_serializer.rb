a21d5d3412e59a72cf3879bcaf4b24e2128472db
Peter Schröder <phoet@posteo.de>
2023-12-11 16:31:31 +0100

    proper enabling

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -3,3 +3,8 @@
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
 Rails.application.config.action_dispatch.cookies_serializer = :json
+
+# https://www.mattlins.com/adding-sign-in-with-apple-to-your-ruby-on-rails-71-app-a-step-by-step-guide
+Rails.application.config.action_dispatch.cookies_same_site_protection = lambda do |request|
+  request.path.starts_with?("/auth/apple") ? :none : :lax
+end
