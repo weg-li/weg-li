4afacd80c479cb2d2e48575973741f6b080dd07e
Peter Schröder <phoet@posteo.de>
2024-01-06 20:25:27 +0100

    bare delete

--- app/jobs/scheduled/data_cleanup_job.rb
+++ app/jobs/scheduled/data_cleanup_job.rb
@@ -7,6 +7,6 @@ class Scheduled::DataCleanupJob < ApplicationJob
     data_sets = DataSet.where("created_at < ?", 6.month.ago)
 
     notify("cleaning up data #{data_sets.count} data_sets")
-    data_sets.destroy_all
+    data_sets.delete_all
   end
 end
