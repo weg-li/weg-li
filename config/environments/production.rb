32d7247ca22bce948ce9176494ee492d4f13afb7
Peter Schröder <phoet@posteo.de>
2024-01-13 21:56:59 +0100

    cable it

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
   config.active_storage.service_urls_expire_in = 1.week
 
   # Mount Action Cable outside main process or domain.
-  config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://www.weg.li/"
+  config.action_cable.mount_path = "/cable"
+  config.action_cable.url = "wss://www.weg.li/cable"
   config.action_cable.allowed_request_origins = [ "https://www.weg.li" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
