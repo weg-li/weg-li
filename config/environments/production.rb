e5dc8ee530702e8278d2ca00487f65bfe477062e
Peter Schröder <phoet@posteo.de>
2024-01-13 21:53:52 +0100

    disable cabe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://www.weg.li/cable"
+  config.action_cable.url = "wss://www.weg.li/"
   config.action_cable.allowed_request_origins = [ "https://www.weg.li" ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
