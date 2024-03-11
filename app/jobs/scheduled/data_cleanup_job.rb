607b8a6ee392bc90b90d818cf323905a3c6ab89b
Peter Schröder <phoet@posteo.de>
2024-03-11 19:52:21 +0100

    cleanup data

--- app/jobs/scheduled/data_cleanup_job.rb
+++ app/jobs/scheduled/data_cleanup_job.rb
@@ -6,7 +6,12 @@ class Scheduled::DataCleanupJob < ApplicationJob
 
     data_sets = DataSet.where("created_at < ?", 6.month.ago)
 
-    notify("cleaning up data #{data_sets.count} data_sets")
+    notify("cleaning up old data #{data_sets.count} data_sets")
+    data_sets.delete_all
+
+    data_sets = DataSet.joins("INNER JOIN notices ON notices.id = setable_id AND notices.status = 3").order(:created_at).where("data_sets.created_at < ?", 1.month.ago).limit(1000)
+
+    notify("cleaning up shared data #{data_sets.count} data_sets")
     data_sets.delete_all
   end
 end
