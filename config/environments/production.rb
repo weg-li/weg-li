8b2e30f4774784a37785f61b69a5722cc11947b0
Peter Schröder <phoet@posteo.de>
2024-03-14 11:38:03 +0100

    set cache headers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,10 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.headers = {
+    "Cache-Control" => "public, max-age=31536000",
+    "Expires" => 1.year.from_now.to_formatted_s(:rfc822)
+  }
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
