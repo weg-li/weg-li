d92ec6abc6df6d6530bd4f7c385751b91117f06b
Peter Schröder <phoet@posteo.de>
2024-03-21 15:38:30 +0100

    send every php request to die

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -13,7 +13,8 @@ Rack::Attack.blocklist('fail2ban pentesters') do |req|
     CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
     req.path.include?('/etc/passwd') ||
     req.path.include?('wp-admin') ||
-    req.path.include?('wp-login')
+    req.path.include?('wp-login') ||
+    req.path.include?('.php')
 
   end
 end
