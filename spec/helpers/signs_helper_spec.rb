a599c12a2b8d8e4c6a616efaa18b70bb79c9d6a4
Peter Schröder <phoet@posteo.de>
2024-11-23 16:45:51 +0100

    fix test

--- spec/helpers/signs_helper_spec.rb
+++ spec/helpers/signs_helper_spec.rb
@@ -17,7 +17,7 @@ describe SignsHelper do
       expect(helper.signify("hi 000.1")).to eql("hi <a class=\"sign\" href=\"/signs/000.1\">000.1</a>")
       expect(helper.signify("hi 000-1")).to eql("hi <a class=\"sign\" href=\"/signs/000-1\">000-1</a>")
       expect(helper.signify("hi 000.1-10")).to eql("hi <a class=\"sign\" href=\"/signs/000.1-10\">000.1-10</a>")
-      expect(helper.signify("hi <000/456>")).to eql("hi <<a class=\"sign\" href=\"/signs/000\">000</a>/<a class=\"sign\" href=\"/signs/456\">456</a>>")
+      expect(helper.signify("hi <000/456>")).to eql("hi &lt;<a class=\"sign\" href=\"/signs/000\">000</a>/<a class=\"sign\" href=\"/signs/456\">456</a>&gt;")
     end
   end
 end
