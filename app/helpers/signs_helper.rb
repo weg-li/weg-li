e4c4b7d46bdcf52fc6439a1147eb9b7cf19dd925
Peter Schröder <phoet@posteo.de>
2024-11-23 10:05:06 +0100

    link images

---
+++ app/helpers/signs_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module SignsHelper
+  SIGNS_REGEX = /(\d{3,4}(\.\d{1,2})?(-\d{1,2})?)/
+
+  def signify(text)
+    return text if text.blank?
+
+    text.gsub(SIGNS_REGEX) do |match|
+      sign = Sign.find_by(number: match)
+      if sign.nil?
+        match
+      elsif !sign.file.exist?
+        link_to(match, sign_path(match), class: "sign")
+      else
+        image = image_tag(asset_url(sign.image), class: "inline-sign", title: sign.description)
+        link_to(match, sign_path(match), title: sign.description, class: "sign") + image
+      end
+    end.html_safe
+  end
+end
---
+++ spec/helpers/signs_helper_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe SignsHelper do
+  context "signify" do
+    it "adds style to show signs" do
+      Fabricate.create(:sign, number: "000", description: "bla")
+      Fabricate.create(:sign, number: "000.1", description: "bla")
+      Fabricate.create(:sign, number: "000-1", description: "bla")
+      Fabricate.create(:sign, number: "000.1-10", description: "bla")
+      Fabricate.create(:sign, number: "456", description: "bla")
+
+      expect(helper.signify(nil)).to eql(nil)
+      expect(helper.signify("hi")).to eql("hi")
+      expect(helper.signify("hi 000")).to eql("hi <a class=\"sign\" href=\"/signs/000\">000</a>")
+      expect(helper.signify("hi 000.1")).to eql("hi <a class=\"sign\" href=\"/signs/000.1\">000.1</a>")
+      expect(helper.signify("hi 000-1")).to eql("hi <a class=\"sign\" href=\"/signs/000-1\">000-1</a>")
+      expect(helper.signify("hi 000.1-10")).to eql("hi <a class=\"sign\" href=\"/signs/000.1-10\">000.1-10</a>")
+      expect(helper.signify("hi <000/456>")).to eql("hi <<a class=\"sign\" href=\"/signs/000\">000</a>/<a class=\"sign\" href=\"/signs/456\">456</a>>")
+    end
+  end
+end
