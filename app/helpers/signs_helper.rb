ae3db25ac511ead6c4f312c9905c5804a2889005
Peter Schröder <phoet@posteo.de>
2024-11-23 16:37:27 +0100

    make sure to render the charge

--- app/helpers/signs_helper.rb
+++ app/helpers/signs_helper.rb
@@ -3,19 +3,22 @@
 module SignsHelper
   SIGNS_REGEX = /(\d{3,4}(\.\d{1,2})?(-\d{1,2})?)/
 
-  def signify(text)
-    return text if text.blank?
+  def signify(text, link: true)
+    Memo::It.memo do
+      return text if text.blank?
 
-    text.gsub(SIGNS_REGEX) do |match|
-      sign = Sign.find_by(number: match)
-      if sign.nil?
-        match
-      elsif !sign.file.exist?
-        link_to(match, sign_path(match), class: "sign")
-      else
-        image = image_tag(asset_url(sign.image), class: "inline-sign", title: sign.description)
-        link_to(match, sign_path(match), title: sign.description, class: "sign") + image
-      end
-    end.html_safe
+      text = h(text)
+      text.gsub(SIGNS_REGEX) do |match|
+        sign = Sign.find_by(number: match)
+        if sign.nil?
+          match
+        elsif !sign.file.exist?
+          link_to_if(link, match, sign_path(match), class: "sign")
+        else
+          image = image_tag(asset_url(sign.image), class: "inline-sign", title: sign.description)
+          link_to_if(link, match, sign_path(match), title: sign.description, class: "sign") + image
+        end
+      end.html_safe
+    end
   end
 end
