ddf373ed57790214d3c1ce96f9e6905fdb4b0ae7
Peter Schröder <phoet@posteo.de>
2024-12-14 10:54:25 +0100

    put percentage in db

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -52,53 +52,9 @@ class Brand < ApplicationRecord
       nil # rubocop:disable Style/ReturnNilInPredicateMethodDefinition
     end
 
+    # TODO: (PS) refactor the brands to use the model
     def percentage(brand)
-      market.dig(brand, 1)
-    end
-
-    def market
-      # https://www.n-tv.de/wirtschaft/VW-bleibt-Marktfuehrer-in-Deutschland-article20883337.html
-      @market ||= {
-        "Volkswagen" => [10_039_389, 21.3],
-        "Opel" => [4_455_662, 9.5],
-        "Mercedes-Benz" => [4_434_329, 9.4],
-        "Ford" => [3_438_207, 7.3],
-        "Audi" => [3_242_838, 6.9],
-        "BMW" => [3_256_884, 6.9],
-        "Škoda" => [2_169_706, 4.6],
-        "Renault" => [1_773_013, 3.8],
-        "Toyota" => [1_302_395, 2.8],
-        "Fiat" => [1_174_960, 2.5],
-        "Hyundai" => [1_195_023, 2.5],
-        "Seat" => [1_154_612, 2.5],
-        "Peugeot" => [1_119_914, 2.4],
-        "Mazda" => [859_054, 1.8],
-        "Nissan" => [863_144, 1.8],
-        "Citroën" => [742_167, 1.6],
-        "Kia" => [662_174, 1.4],
-        "Dacia" => [547_617, 1.2],
-        "Suzuki" => [502_393, 1.1],
-        "Honda" => [451_525, 1.0],
-        "Mitsubishi" => [484_017, 1.0],
-        "Smart" => [474_898, 1.0],
-        "Volvo" => [489_040, 1.0],
-        "MINI" => [445_226, 0.9],
-        "Sonstige" => [415_633, 0.9],
-        "Porsche" => [313_173, 0.7],
-        "Chevrolet" => [214_604, 0.5],
-        "Alfa Romeo" => [119_095, 0.3],
-        "Subaru" => [123_887, 0.3],
-        "Daihatsu" => [78_619, 0.2],
-        "Jaguar" => [73_932, 0.2],
-        "Jeep" => [112_467, 0.2],
-        "Land Rover" => [108_056, 0.2],
-        "Chrysler" => [59_001, 0.1],
-        "DS" => [33_607, 0.1],
-        "Lancia" => [28_860, 0.1],
-        "Lexus" => [27_282, 0.1],
-        "MG Rover" => [28_777, 0.1],
-        "Saab" => [44_345, 0.1],
-      }
+      from_param(brand).share
     end
 
     def from_param(param)
@@ -112,9 +68,5 @@ class Brand < ApplicationRecord
 
   def defaults
     self.token ||= to_param
-  end
-
-  def market_cap
-    self.class.percentage(name)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_04_104348) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_14_093628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
@@ -72,8 +72,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_104348) do
     t.datetime "updated_at", null: false
     t.integer "status", default: 0, null: false
     t.string "falsepositives", default: [], null: false, array: true
+    t.decimal "share"
     t.index ["name"], name: "index_brands_on_name"
-    t.index ["token"], name: "index_brands_on_token"
+    t.index ["token"], name: "index_brands_on_token", unique: true
   end
 
   create_table "bulk_uploads", force: :cascade do |t|
