c28c49f7bfa5948c5e96d1d083cfdee6c8bb5760
Peter Schröder <phoet@posteo.de>
2025-01-13 12:19:04 +0100

    change prio

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -5,6 +5,6 @@ class Snippet < ApplicationRecord
 
   validates :name, :content, presence: true
 
-  scope :ordered, -> { order(:priority, :id) }
+  scope :ordered, -> { order(priority: :desc, created_at: :asc) }
   scope :search, ->(term) { where("name ILIKE :term OR content ILIKE :term", term: "%#{term}%") }
 end
--- spec/models/snippet_spec.rb
+++ spec/models/snippet_spec.rb
@@ -10,4 +10,17 @@ describe Snippet do
       expect(snippet).to be_valid
     end
   end
+
+  context "priority" do
+    it "sorts according to priority, the higher the better" do
+      last = Fabricate.create(:snippet, priority: -1)
+      fifth = Fabricate.create(:snippet)
+      second = Fabricate.create(:snippet, priority: 9)
+      first = Fabricate.create(:snippet, priority: 10)
+      third = Fabricate.create(:snippet, priority: 8)
+      fourth = Fabricate.create(:snippet, priority: 8)
+
+      expect(Snippet.ordered).to match([first, second, third, fourth, fifth, last])
+    end
+  end
 end
