aed9c32bb373557c8f20968606bf68615211fb64
Peter Schröder <phoet@posteo.de>
2025-01-17 10:27:03 +0100

    use strict args

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,5 +3,3 @@ require 'sidekiq'
 require 'sidekiq/scheduler'
 require 'sidekiq/web'
 require 'sidekiq-scheduler/web'
-
-Sidekiq.strict_args!(false) # https://github.com/hotwired/turbo-rails/issues/535
