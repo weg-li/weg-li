38557a201f3b9f8c005fe7bb271d2791c518ee75
Peter Schröder <phoet@posteo.de>
2025-01-22 13:52:52 +0100

    use selective regex

--- config/routes.rb
+++ config/routes.rb
@@ -112,7 +112,7 @@ Rails.application.routes.draw do
 
   resources :districts
   resources :signs, only: %i[index] do
-    member { get :show, constraints: { id: /[^\/]+/ } }
+    member { get :show, constraints: { id: /\d+\.?\d?/ } }
   end
   resources :charges, only: %i[index show] do
     collection { get :list }
