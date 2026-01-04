f2580177f7c6a2733882de08e955abb7be55f4e5
Peter Schröder <phoet@posteo.de>
2026-01-04 17:42:58 +0100

    fix missing content

--- app/mailboxes/autoreply_mailbox.rb
+++ app/mailboxes/autoreply_mailbox.rb
@@ -13,7 +13,7 @@ class AutoreplyMailbox < ApplicationMailbox
       action_mailbox_inbound_email: inbound_email,
       sender: mail.from.first,
       subject: mail.subject || "-",
-      content: content_from_mail(mail) || "-",
+      content: content_from_mail(mail).presence || "-",
     }
     reply = notice.replies.create!(params)
 
