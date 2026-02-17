2bfbf1d5b40722d7d9f155fbec0547709c663aa6
Peter Schröder <phoet@posteo.de>
2026-02-17 12:14:51 +0000

    no warning

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -49,7 +49,7 @@ class Brand < ApplicationRecord
         res = in_brands?(text, send(kind))
         return res if res
       end
-      nil # rubocop:disable Style/ReturnNilInPredicateMethodDefinition
+      nil
     end
 
     def from_param(param)
