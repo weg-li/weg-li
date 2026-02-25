38a325e1fbb3334ff0688102eda0a76ed429acf2
Peter Schröder <phoet@posteo.de>
2026-02-25 22:34:57 +0100

    no check

--- entrypoints/render-build.sh
+++ entrypoints/render-build.sh
@@ -5,6 +5,3 @@ set -o errexit
 bundle install
 yarn install
 NODE_OPTIONS=--openssl-legacy-provider bundle exec rails assets:precompile
-
-test -f public/assets/.manifest.json
-grep -q '"application.css"' public/assets/.manifest.json
