b1c9e9cc3ebeea9273091e70f5b2909417ebd21d
Peter Schröder <phoet@posteo.de>
2026-02-25 22:32:38 +0100

    try this

--- entrypoints/render-build.sh
+++ entrypoints/render-build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 bundle install
 yarn install
-RAILS_ENV=production SECRET_KEY_BASE_DUMMY=1 NODE_OPTIONS=--openssl-legacy-provider bundle exec rails assets:precompile
+NODE_OPTIONS=--openssl-legacy-provider bundle exec rails assets:precompile
 
 test -f public/assets/.manifest.json
 grep -q '"application.css"' public/assets/.manifest.json
