6c04f76af5bd774c614667e5ab2b96ded8849596
Peter Schröder <phoet@posteo.de>
2026-03-08 10:39:26 +0100

    destroy trash

--- app/jobs/scheduled/notice_archiver_job.rb
+++ app/jobs/scheduled/notice_archiver_job.rb
@@ -2,10 +2,15 @@
 
 class Scheduled::NoticeArchiverJob < ApplicationJob
   def perform
-    Rails.logger.info "archiving notices"
+    duration = 4.years.ago
+    Rails.logger.info "archiving notices since #{duration}"
 
-    notices = Notice.where("created_at < ?", 4.years.ago).where(archived: false)
+    notices = Notice.where("created_at < ?", duration).where(archived: false)
     notify "archiving #{notices.count} notices"
     notices.update_all(archived: true)
+
+    notices = Notice.where("created_at < ?", duration).not_shared
+    notify "deleting #{notices.count} trash notices"
+    notices.destroy_all
   end
 end
--- spec/jobs/scheduled/notice_archiver_job_spec.rb
+++ spec/jobs/scheduled/notice_archiver_job_spec.rb
@@ -5,13 +5,17 @@ require "spec_helper"
 describe Scheduled::NoticeArchiverJob do
   context "perform" do
     it "should archive notices" do
-      Fabricate.create(:notice, created_at: 4.years.ago)
-      Fabricate.create(:notice, created_at: 3.years.ago)
-      Fabricate.create(:notice, created_at: 2.years.ago)
+      Fabricate.create(:notice, created_at: 5.years.ago, status: :open)
+      Fabricate.create(:notice, created_at: 5.years.ago, status: :shared)
+      Fabricate.create(:notice, created_at: 4.years.ago, status: :shared)
+      Fabricate.create(:notice, created_at: 3.years.ago, status: :shared)
+      Fabricate.create(:notice, created_at: 2.years.ago, status: :shared)
 
       expect do
-        Scheduled::NoticeArchiverJob.perform_now
-      end.to change { Notice.where(archived: true).count }.by(1)
+        expect do
+          Scheduled::NoticeArchiverJob.perform_now
+        end.to change { Notice.where(archived: true).count }.by(2)
+      end.to change { Notice.count }.by(-1)
     end
   end
 end
